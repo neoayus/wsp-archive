3d8251572f6500c4182f251528cb0ca5da00c6c1
neoayus <ayus43@outlook.com>
2026-03-23 18:17:17 +0530

    initialize project structure

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+import '../styles/Sidebar.css'
+
+export default function Sidebar(){
+  return(
+    <nav className="sidebar">
+      <p>Slime.</p>
+      <div className='icon'></div>
+    </nav>
+  )
+}
