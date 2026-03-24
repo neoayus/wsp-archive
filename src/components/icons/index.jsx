c553c99b961eb9b66d8836840a52d15f49501760
neoayus <ayus43@outlook.com>
2026-03-24 09:24:28 +0530

    chore: add custom svgs

---
+++ src/components/icons/index.jsx
@@ -0,0 +1,14 @@
+// barrel file for icons 
+
+export {default as Chat} from "./Chat";
+export {default as Status} from './Status';
+export {default as Channel} from './Channel';
+export {default as Communities} from './Communities';
+export {default as Media} from './Media';
+export {default as User} from './User';
+export {default as Whatsapp} from './Whatsapp';
+export {default as NewChat} from './NewChat';
+export {default as Add} from "./Add";
+export {default as Search} from "./Search";
+export {default as Microphone} from "./Microphone";
+export {default as Sticker} from "./Sticker";
